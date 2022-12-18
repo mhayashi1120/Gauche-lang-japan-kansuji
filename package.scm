6e377fe618b6955dc558ff20f78f9745ea1b6fd3
Masahiro Hayashi <mhayashi1120@gmail.com>
2022-12-18 23:19:03 +0900

    upgrade revision

--- package.scm
+++ package.scm
@@ -4,7 +4,7 @@
 
 (define-gauche-package "Gauche-lang-japan-kansuji"
   ;;
-  :version "0.5.6"
+  :version "0.5.7"
 
   ;; Description of the package.  The first line is used as a short
   ;; summary.
