02bfa0405e01bbfbb333dfa97b9c3678426a8977
Masahiro Hayashi <mhayashi1120@gmail.com>
2022-12-19 13:47:15 +0900

    revup

--- package.scm
+++ package.scm
@@ -4,7 +4,7 @@
 
 (define-gauche-package "Gauche-lang-japan-kansuji"
   ;;
-  :version "0.5.7"
+  :version "0.5.8"
 
   ;; Description of the package.  The first line is used as a short
   ;; summary.
