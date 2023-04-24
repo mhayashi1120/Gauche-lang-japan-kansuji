a1d8544ada97b3ebd7022273b56910b0329bfe67
Masahiro Hayashi <mhayashi1120@gmail.com>
2023-04-24 17:09:29 +0900

    add exclusion revup

--- package.scm
+++ package.scm
@@ -4,7 +4,7 @@
 
 (define-gauche-package "Gauche-lang-japan-kansuji"
   ;;
-  :version "0.5.11"
+  :version "0.5.12"
 
   ;; Description of the package.  The first line is used as a short
   ;; summary.
